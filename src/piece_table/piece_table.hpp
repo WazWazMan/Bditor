c45da053f92811d9cd3583c1b4156e7f4754f421
Daniel <WazWazMan1@gmail.com>
2025-09-08 20:03:05 +0300

    Add initial declarations for piece table data structure

--- src/piece_table/piece_table.hpp
+++ src/piece_table/piece_table.hpp
@@ -1,1 +1,71 @@
 #pragma once
+#include <vector>
+#include <deque>
+#include <string>
+#include <memory>
+
+class PieceTable
+{
+private:
+    enum Color
+    {
+        RED,
+        BLACK
+    };
+    struct BufferPosition
+    {
+        int index;
+        int offset;
+
+        BufferPosition(int index, int offset);
+    };
+    struct EditPiece
+    {
+        int bufferInfex;
+        BufferPosition start;
+        BufferPosition end;
+
+        int leftSubTreeLength;
+        int leftSubTreeLineCount;
+
+        EditPiece(const int bufferInfex, const BufferPosition &start, const BufferPosition &end);
+    };
+    struct EditNode
+    {
+        EditPiece data;
+
+        Color color;
+        EditNode *parent;
+        EditNode *left;
+        EditNode *right;
+
+        EditNode(EditPiece &data);
+        ~EditNode();
+    };
+    struct Buffer
+    {
+        std::string str;
+        int *lineStarts;
+
+        Buffer(std::string str);
+        ~Buffer();
+    };
+
+    EditNode *editTreeRoot;
+    std::vector<std::unique_ptr<Buffer>> buffers;
+
+    void change(const unsigned int &index, const unsigned int length, const std::string &data);
+    int insertBuffer(std::string &data);
+    void insertEdit(EditNode data);
+    EditNode &findEdit(int &index);
+
+public:
+    PieceTable();
+    ~PieceTable();
+
+    PieceTable &insert(const unsigned int &index, const std::string &data);
+    PieceTable &remove(const unsigned int &index, const unsigned int &length);
+    PieceTable &replace(const unsigned int &index, const unsigned int &length, const std::string &data);
+
+    std::string getLineContent();
+};
